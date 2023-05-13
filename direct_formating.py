b5f077184856bce5ae750ca062eeb4854813c09c
Tolani99 <tolaniadisa999@gmail.com>
2023-05-13 23:09:07 +0100

    Standard pylint code

--- direct_formating.py
+++ direct_formating.py
@@ -1,3 +1,11 @@
-print('Adding the values of {} and {} = {}'.format(a, b, ans))
+"""
+Showing how to add values
+"""
+
+A = 23
+B = 32
+ANS = A + B
+
+print(f'Adding the values of {A} and {B} = {ANS}')
 # Output
 # Adding the values of 10 and 5 = 15
