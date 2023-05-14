75dc1f53053c92417f208e14f272a4dfdca453d5
Tolani99 <tolaniadisa999@gmail.com>
2023-05-14 11:50:09 +0100

    Using break statements in loops

---
+++ week_1/class_activity/control_flow.py/looping_statement/use_of_break_in_loops.py
@@ -0,0 +1,10 @@
+#Starter Code
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisú', 'Chocolate Cake']
+
+for dessert in favorites:
+    if dessert == 'Pudding':
+        print('Yes one of my favorite desserts is', dessert)
+        break 
+else:
+    print('No sorry, not a dessert on my list')
+
