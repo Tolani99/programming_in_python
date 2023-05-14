4220fb6a438d1e31c61e6429503e71bec1766321
Tolani99 <tolaniadisa999@gmail.com>
2023-05-14 11:50:47 +0100

    Using continue statement in loops

---
+++ week_1/class_activity/control_flow.py/looping_statement/using_continue_in_loops.py
@@ -0,0 +1,8 @@
+#Starter Code
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisú', 'Chocolate Cake']
+
+for dessert in favorites:
+    if dessert == 'Churros':
+        continue
+    print('Other desserts I like are', dessert) 
+
