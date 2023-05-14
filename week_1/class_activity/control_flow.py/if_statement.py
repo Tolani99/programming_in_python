361785587122a3cb09eeedeb62c541e15d1e5c86
Tolani99 <tolaniadisa999@gmail.com>
2023-05-14 09:23:55 +0100

    Using if statement

--- week_1/class_activity/control_flow.py/if_statement.py
+++ week_1/class_activity/control_flow.py/if_statement.py
@@ -1,12 +1,17 @@
-bill_total = 210
+"""
+A program to demonstrate the use
+of if statements
+"""
 
-discount1 = 10
-discount2 = 20
+BILL_TOTAL= 210
 
-if bill_total > 100 and bill_total < 200:
+DISCOUNT1 = 10
+DISCOUNT2 = 20
+
+if BILL_TOTAL > 100 and BILL_TOTAL < 200:
     print("Bill is greater than 100!")
-    bill_total = bill_total - discount1
+    BILL_TOTAL = BILL_TOTAL - DISCOUNT1
 else:
     print("Bill is less than 100!")
-print("Total bill: " + str(bill_total))
 
+print("Total bill: " + str(BILL_TOTAL))
