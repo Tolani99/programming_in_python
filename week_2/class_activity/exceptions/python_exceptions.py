27f859070b9194a959cd31f02dcc9c57fa94181b
Tolani99 <tolaniadisa999@gmail.com>
2023-05-15 16:27:57 +0100

    Using try and except on zero divisio error

--- week_2/class_activity/exceptions/python_exceptions.py
+++ week_2/class_activity/exceptions/python_exceptions.py
@@ -2,5 +2,6 @@ def divide_by(a, b):
     return a / b
 try:
     ans = divide_by(40, 0)
-except:
-    print('Something went wrong!')
+except Exception as e:
+    print('Something went wrong!', e)
+    print(e.__class__)
