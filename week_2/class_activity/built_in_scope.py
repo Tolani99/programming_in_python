3b996966606fb62935f278fd556c9165a5dedb4d
Tolani99 <tolaniadisa999@gmail.com>
2023-05-15 09:03:53 +0100

    Using key word arguments

---
+++ week_2/class_activity/built_in_scope.py
@@ -0,0 +1,4 @@
+"""
+4. Built-in scope
+Built-in scope refers to the reserved keywords that Python uses for its built-in functions, such as print, def, for, in, and so forth.  Functions with built-in scope can be accessed at any level.
+"""
