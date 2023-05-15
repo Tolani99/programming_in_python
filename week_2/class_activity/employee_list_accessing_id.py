311333b1cb6a8a9c359af5780fd69700d775793c
Tolani99 <tolaniadisa999@gmail.com>
2023-05-15 11:49:52 +0100

    Employee list, accessing id

--- week_2/class_activity/employee_list_accessing_id.py
+++ week_2/class_activity/employee_list_accessing_id.py
@@ -6,11 +6,19 @@ In this example, there's a list of employees that work in a restaurant. You need
 employee_list = [{"id": 12345, "name": "John", "department": "Kitchen"}, {"id": 12458, "name": "Paul", "department": "House Floor"}]
 
 def get_employee(id): 
-    for employee in employee_list:
+    for employee in employee_list:0
         if employee['id'] == id:
             return employee
 
 print(get_employee(12458));
 # OUTPUT
 #{'id': 12458, 'name': 'Paul', 'department': 'House Floor'}
+
+"""
+The code runs well and will return the user Paul, as its ID, 12458, is matched. The challenge comes when the list gets bigger.
+
+Instead of two employees, you may have 2000 or even 20,000. The code will have to iterate over the list sequentially until the number is matched.
+
+You could optimize the code to split the search, but even with this, it still lacks in performance in comparison to other data structures, such as the dictionary.
+"""
 
