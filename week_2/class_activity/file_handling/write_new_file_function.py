f283a946580f9387d9655885bfb2b2162e8dbf37
Tolani99 <tolaniadisa999@gmail.com>
2023-05-17 13:09:25 +0100

    Write new file lines function

---
+++ week_2/class_activity/file_handling/write_new_file_function.py
@@ -0,0 +1,2 @@
+with open('newfile.txt', 'w') as file:
+    print(file.write('This is a new file created!'))
---
+++ week_2/class_activity/file_handling/write_new_file_lines_function.py
@@ -0,0 +1,2 @@
+with open('newlycreated.txt', 'w') as file:
+    file.writelines(['This is the newly created test file', 'This is another line to be added.'])
