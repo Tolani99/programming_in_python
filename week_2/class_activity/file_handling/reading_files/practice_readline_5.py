aa78665b40b8d1718f790ca62cf1cea3fbc3c099
Tolani99 <tolaniadisa999@gmail.com>
2023-05-19 13:28:21 +0100

    Using readline in python

--- week_2/class_activity/file_handling/reading_files/practice_readline_5.py
+++ week_2/class_activity/file_handling/reading_files/practice_readline_5.py
@@ -3,3 +3,15 @@ Returns the first line
 """
 with open('testing.txt', 'r') as file:
     print(file.readlines())
+
+"""
+The code snippet provided demonstrates how to read the contents of a file named 'testing.txt' using Python.
+
+Here's a breakdown of the code:
+
+with open('testing.txt', 'r') as file:: This line opens the file named 'testing.txt' in read mode ('r') using the open() function. The with statement ensures that the file is automatically closed after the code block is executed.
+
+print(file.readlines()): This line reads all the lines in the file and returns them as a list of strings using the readlines() method. The print() function is then used to output the contents of the file to the console.
+
+In summary, the code snippet opens the 'testing.txt' file, reads all the lines from it, and then prints them to the console.
+"""
