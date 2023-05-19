e727ce68fa2f9df10f69e8394b34b4863866497f
Tolani99 <tolaniadisa999@gmail.com>
2023-05-19 15:03:21 +0100

    Addiming doc string to the code lines

--- week_2/class_activity/file_handling/reading_files/practice_readline_9_from_multiple_folder.py
+++ week_2/class_activity/file_handling/reading_files/practice_readline_9_from_multiple_folder.py
@@ -4,3 +4,19 @@ f = open(f_name) # "r" omitted as it's the default
 f_content = f.read()
 f_content_list = f_content.split("\n")
 print(random.choice(f_content_list))
+"""
+
+import random: This line imports the random module, which provides functions for generating random numbers and making random selections.
+
+f_name = input('Type the file name: '): This line prompts the user to enter the name of the file they want to read. The input is stored in the variable f_name.
+
+f = open(f_name): This line opens the file specified by the user in read mode ("r") and assigns the file object to the variable f. Note that "r" is the default mode, so it can be omitted.
+
+f_content = f.read(): This line reads the entire content of the file and stores it as a string in the variable f_content.
+
+f_content_list = f_content.split("\n"): This line splits the content string into a list of lines using the newline character ("\n") as the delimiter. Each line is an element in the list f_content_list.
+
+print(random.choice(f_content_list)): This line randomly selects one element (line) from the f_content_list using the random.choice() function and then prints it. This will output a random line from the file.
+
+Please note that this code assumes that the file specified by the user exists and is accessible. Additionally, it's a good practice to close the file after reading by adding f.close() after print(random.choice(f_content_list)). Alternatively, you can use the with statement as shown in the previous examples to automatically close the file after reading.
+"""
