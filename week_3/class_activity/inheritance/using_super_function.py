a3e984ca3422b120a431596155e21087b5f753ac
Tolani99 <tolaniadisa999@gmail.com>
2023-05-24 13:28:20 +0100

    Using super function in obeject oriented programming

---
+++ week_3/class_activity/inheritance/using_super_function.py
@@ -0,0 +1,18 @@
+"""
+The super() function is a built-in function that can be called inside the derived class and gives access to the methods and variables of the parent classes or sibling classes. Sibling classes are the classes that share the same parent class. When you call the super() function, you get an object that represents the parent class in return.
+
+The super() function plays an important role in multiple inheritance and helps drive the flow of the code execution. It helps in managing or determining the control of from where I can draw the values of my desired functions and variables.
+
+If you change anything inside the parent class, there is a direct retrieval of changes inside the derived class. This is mainly used in places where you need to initialize the functionalities present inside the parent class in the child class as well. You can then add additional code in the child class.
+"""
+class Fruit():
+    def __init__(self, fruit):
+        print('Fruit type: ', fruit)
+
+
+class FruitFlavour(Fruit):
+    def __init__(self):
+        super().__init__('Apple')
+        print('Apple is sweet')
+
+apple = FruitFlavour()
