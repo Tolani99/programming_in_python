c7a57aedb1033659ea86ae269d06861166cf3d7a
Tolani99 <tolaniadisa999@gmail.com>
2023-05-26 08:52:39 +0100

    Using file changes yprint content

--- week_4/class_activity/reload_function/reload.py
+++ week_4/class_activity/reload_function/reload.py
@@ -1,1 +1,7 @@
 import importlib
+import filechanges
+
+def print_changes():
+    contents = os.listdir(r'C:\Users\tolani.adisa_maxdriv\github_work\data_engineering_coursera\programming_in_python\week_4\class_activity\reload_function')
+
+    print(contents)
