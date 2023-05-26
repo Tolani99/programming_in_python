424b427d4b9bb518293bb37543efcff7ed566644
Tolani99 <tolaniadisa999@gmail.com>
2023-05-26 08:50:37 +0100

    Importing os for displaying files

---
+++ week_4/class_activity/reload_function/filechanges.py
@@ -0,0 +1,4 @@
+import os
+
+contents = os.listdir(r'C:\Users\tolani.adisa_maxdriv\github_work\data_engineering_coursera\programming_in_python\week_4\class_activity\reload_function')
+print(contents)
