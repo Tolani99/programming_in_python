9c298501386dbac2163145a1a7bdbaa844bd05cf
Tolani99 <tolaniadisa999@gmail.com>
2023-05-26 10:12:28 +0100

    Additions to the reload function

--- week_4/class_activity/reload_function/filechanges.py
+++ week_4/class_activity/reload_function/filechanges.py
@@ -1,4 +1,5 @@
 import os
 
-contents = os.listdir(r'C:\Users\tolani.adisa_maxdriv\github_work\data_engineering_coursera\programming_in_python\week_4\class_activity')
+contents = os.listdir(r'C:\Users\tolani.adisa_maxdriv\github_work\data_engineering_coursera\programming_in_python\week_4')
+print("Current directory content:")
 print(contents)
--- week_4/class_activity/reload_function/reloads.py
+++ week_4/class_activity/reload_function/reloads.py
@@ -1,5 +1,15 @@
 import importlib
-import sample
+import filechanges
 
 # importlib.reload(sample)
 # importlib.reload(sample)
+
+def changes():
+    try:
+        importlib.reload(filechanges)
+        filechanges.print_changes()
+    except:
+        pass
+for i in range(5):
+    changes()
+    input("Hit enter to reload ...")
