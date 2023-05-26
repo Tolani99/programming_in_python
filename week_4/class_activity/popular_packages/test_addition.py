f3887243f46be08564201baad2ac10fedc14a4ed
Tolani99 <tolaniadisa999@gmail.com>
2023-05-26 17:10:24 +0100

    Additional readings

--- week_4/class_activity/popular_packages/test_addition.py
+++ week_4/class_activity/popular_packages/test_addition.py
@@ -6,3 +6,13 @@ def test_add():
 
 def test_sub():
     assert additions.sub(4, 5) == -1
+
+# #python3 -m pytest test_addition.py -v
+# -v for verbose
+# -q quiet mode
+# -s allows the print statement inside the functions to be executed
+# -x is to flag the tests to stop execution after first failure
+# -m is used to mark a specific function
+# -k is a flag for searching and running tests with a specific keyword
+# --tb is to disable the traceback code of errors
+# --maxfail n specifies maximum number of test fails allowed
