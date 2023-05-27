4db6cec3f0c0f1b0c6059051687c603a4a83c440
Tolani99 <tolaniadisa999@gmail.com>
2023-05-27 07:04:16 +0100

    Work on class activity

---
+++ week_4/class_activity/accessing_modules/mycalendar.py
@@ -0,0 +1,12 @@
+# import sys
+# location = sys.path
+# print(location)
+# for i in location:
+#     print(i)
+
+import calendar
+
+leapdays = calendar.leapdays(2000, 2050)
+print(leapdays)
+isitleap = calendar.isleap(2036)
+print(isitleap)
--- week_4/class_activity/reload_function/reload.py
+++ week_4/class_activity/reload_function/reload.py
@@ -1,6 +1,6 @@
 import importlib
 import filechanges
-
+import os
 def print_changes():
     contents = os.listdir(r'C:\Users\tolani.adisa_maxdriv\github_work\data_engineering_coursera\programming_in_python\week_4\class_activity\reload_function')
     print('Current directory contents:')
